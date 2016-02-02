836e7e45ceea4efe95b415c77b6dbc5f91a9d601
maxwell <maxwell.house@daumkakao.com>
2016-02-02 17:17:55 +0900

    .

--- src/main/java/osy/algorithm/CountTwo.java
+++ src/main/java/osy/algorithm/CountTwo.java
@@ -4,11 +4,20 @@ package osy.algorithm;
  * Created by maxwell on 2016-02-02.
  */
 public class CountTwo {
+    // O(n) solution
     public static int countTwo(int n) {
-        if (n == 1) return 0;
-        return countTwo(n - 1) + countTwoOneNumber(n);
+        if (n <= 1) return 0;
+        
+        int temp = 0;
+        int count = 0;
+        
+        for (int i = 2; i <= n; ++i) {
+            count = temp + countTwoOneNumber(i);
+            temp = count;           
+        }
+        return count;
     }
-    
+
     private static int countTwoOneNumber(int num) {
         int count = 0;
         while (num > 0) {
@@ -17,4 +26,21 @@ public class CountTwo {
         }
         return count;
     }
+    
+    // O(log n) solution
+    public static int countTwo2(int n) {
+        if (n <= 1) return 0;
+        if (n <= 9) return 1;
+        
+        if (n % 10 == 9) {  // [0, n] 의 개수가 10의 배수
+            return 10 * countTwo2(n / 10) + (n / 10 + 1);    
+        }
+        
+        int d = n - (n % 10 + 1);
+        int count = countTwo2(d) + (n - d) * countTwoOneNumber(d + 1);
+        if (n % 10 >= 2) {
+            count += 1;
+        }
+        return count;
+    }    
 }
--- src/main/java/osy/test/CountTwoTest.java
+++ src/main/java/osy/test/CountTwoTest.java
@@ -8,20 +8,74 @@ import osy.algorithm.CountTwo;
  */
 public class CountTwoTest {
     @Test
-    public void testCountTwo1() {
+    public void testCountTwoA1() {
         int n = 1;        
         Assert.assertEquals(0, CountTwo.countTwo(n));
     }
 
     @Test
-    public void testCountTwo2() {
+    public void testCountTwoA2() {
         int n = 2;
         Assert.assertEquals(1, CountTwo.countTwo(n));
     }
 
     @Test
-    public void testCountTwo3() {
+    public void testCountTwoA3() {
         int n = 22;
         Assert.assertEquals(6, CountTwo.countTwo(n));
+    }
+
+    @Test
+    public void testCountTwoB1() {
+        int n = 1;
+        Assert.assertEquals(0, CountTwo.countTwo2(n));
+    }
+
+    @Test
+    public void testCountTwoB2() {
+        int n = 2;
+        Assert.assertEquals(1, CountTwo.countTwo2(n));
+    }
+
+    @Test
+    public void testCountTwoB3() {
+        int n = 22;
+        Assert.assertEquals(6, CountTwo.countTwo2(n));
+    }
+
+    @Test
+    public void testCountTwoAandB() {
+        int n = 0;
+        try {
+            for (; n <= 50000; ++n) {
+                if (n % 5000 == 0) {
+                    System.out.println("n=" + n);
+                }
+                Assert.assertEquals(CountTwo.countTwo(n), CountTwo.countTwo2(n));
+            }
+        } catch(Exception e) {
+            System.out.println(e);
+            System.out.println("n=" + n);
+        }
+    }
+
+    @Test
+    public void testCountTwoAPerformance() {
+        System.out.println("---- O(n) solution ----");
+        for (int i = 0; i < 10000000; i += 100000) {
+            long startTime = System.currentTimeMillis();
+            CountTwo.countTwo(i);
+            long endTime = System.currentTimeMillis();
+            System.out.println("n=" + i + ": " + (endTime - startTime) + " ms");
+        }
+        
+        System.out.println();
+        System.out.println("---- O(log n) solution ----");
+        for (int i = 0; i < 10000000; i += 100000) {
+            long startTime = System.currentTimeMillis();
+            CountTwo.countTwo2(i);
+            long endTime = System.currentTimeMillis();
+            System.out.println("n=" + i + ": " + (endTime - startTime) + " ms");
+        }
     }
 }
