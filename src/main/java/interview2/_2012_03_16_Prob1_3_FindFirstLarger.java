064ea51f461fa061722e62d79d1751fed38815e4
maxwell.house <maxwell@daumkakao.com>
2016-03-05 22:41:18 +0900

    .

--- src/main/java/interview2/_2012_03_16_Prob1_3_FindFirstLarger.java
+++ src/main/java/interview2/_2012_03_16_Prob1_3_FindFirstLarger.java
@@ -15,4 +15,18 @@ public class _2012_03_16_Prob1_3_FindFirstLarger {
 			else return tryLeft;
 		}
 	}
+	
+	////////////////////////////////////////////////
+	public static int findFirstLarger2(int[] arr, int k) {
+        if (arr[arr.length - 1] <= k) return -1;
+		return findLarger2(arr, k, 0, arr.length - 1);
+	}
+
+	private static int findLarger2(int[] arr, int k, int l, int r) {
+        if (l == r) return l;
+        
+		int m = (l + r) / 2;
+		if (arr[m] <= k ) return findLarger2(arr, k, m + 1, r);
+		else return findLarger2(arr, k, l, m);			
+	}
 }
--- src/main/java/test2/test_2012_03_16_Prob1_3_FindFirstLarger.java
+++ src/main/java/test2/test_2012_03_16_Prob1_3_FindFirstLarger.java
@@ -14,7 +14,7 @@ public class test_2012_03_16_Prob1_3_FindFirstLarger {
 		int actual = _2012_03_16_Prob1_3_FindFirstLarger.findFirstLarger(arr, k);
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testFindFirstLarger2() throws Exception {
 		int[] arr = {0,1,2,3,4,5};
@@ -23,7 +23,7 @@ public class test_2012_03_16_Prob1_3_FindFirstLarger {
 		int actual = _2012_03_16_Prob1_3_FindFirstLarger.findFirstLarger(arr, k);
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testFindFirstLarger3() throws Exception {
 		int[] arr = {0,1,2,3,4,5};
@@ -32,7 +32,7 @@ public class test_2012_03_16_Prob1_3_FindFirstLarger {
 		int actual = _2012_03_16_Prob1_3_FindFirstLarger.findFirstLarger(arr, k);
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testFindFirstLarger4() throws Exception {
 		int[] arr = {0,1,2,3,4,5};
@@ -41,7 +41,7 @@ public class test_2012_03_16_Prob1_3_FindFirstLarger {
 		int actual = _2012_03_16_Prob1_3_FindFirstLarger.findFirstLarger(arr, k);
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testFindFirstLarger5() throws Exception {
 		int[] arr = {0,10,20,30,40};
@@ -50,7 +50,7 @@ public class test_2012_03_16_Prob1_3_FindFirstLarger {
 		int actual = _2012_03_16_Prob1_3_FindFirstLarger.findFirstLarger(arr, k);
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testFindFirstLarger6() throws Exception {
 		int[] arr = {0,10,20,30,40};
@@ -59,7 +59,7 @@ public class test_2012_03_16_Prob1_3_FindFirstLarger {
 		int actual = _2012_03_16_Prob1_3_FindFirstLarger.findFirstLarger(arr, k);
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testFindFirstLarger7() throws Exception {
 		int[] arr = {0};
@@ -68,7 +68,7 @@ public class test_2012_03_16_Prob1_3_FindFirstLarger {
 		int actual = _2012_03_16_Prob1_3_FindFirstLarger.findFirstLarger(arr, k);
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testFindFirstLarger8() throws Exception {
 		int[] arr = {0};
@@ -77,13 +77,94 @@ public class test_2012_03_16_Prob1_3_FindFirstLarger {
 		int actual = _2012_03_16_Prob1_3_FindFirstLarger.findFirstLarger(arr, k);
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testFindFirstLarger9() throws Exception {
 		int[] arr = {0};
 		int k = 0;
 		int expected = -1;
 		int actual = _2012_03_16_Prob1_3_FindFirstLarger.findFirstLarger(arr, k);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testFindFirstLarger10() throws Exception {
+		int[] arr = {0,1,2,3,4,5};
+		int k = 6;
+		int expected = -1;
+		int actual = _2012_03_16_Prob1_3_FindFirstLarger.findFirstLarger2(arr, k);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testFindFirstLarger11() throws Exception {
+		int[] arr = {0,1,2,3,4,5};
+		int k = 5;
+		int expected = -1;
+		int actual = _2012_03_16_Prob1_3_FindFirstLarger.findFirstLarger2(arr, k);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testFindFirstLarger12() throws Exception {
+		int[] arr = {0,1,2,3,4,5};
+		int k = 0;
+		int expected = 1;
+		int actual = _2012_03_16_Prob1_3_FindFirstLarger.findFirstLarger2(arr, k);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testFindFirstLarger13() throws Exception {
+		int[] arr = {0,1,2,3,4,5};
+		int k = -1;
+		int expected = 0;
+		int actual = _2012_03_16_Prob1_3_FindFirstLarger.findFirstLarger2(arr, k);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testFindFirstLarger14() throws Exception {
+		int[] arr = {0,10,20,30,40};
+		int k = 20;
+		int expected = 3;
+		int actual = _2012_03_16_Prob1_3_FindFirstLarger.findFirstLarger2(arr, k);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testFindFirstLarger15() throws Exception {
+		int[] arr = {0,10,20,30,40};
+		int k = 25;
+		int expected = 3;
+		int actual = _2012_03_16_Prob1_3_FindFirstLarger.findFirstLarger2(arr, k);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testFindFirstLarger16() throws Exception {
+		int[] arr = {0};
+		int k = 25;
+		int expected = -1;
+		int actual = _2012_03_16_Prob1_3_FindFirstLarger.findFirstLarger2(arr, k);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testFindFirstLarger17() throws Exception {
+		int[] arr = {0};
+		int k = -1;
+		int expected = 0;
+		int actual = _2012_03_16_Prob1_3_FindFirstLarger.findFirstLarger2(arr, k);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testFindFirstLarger18() throws Exception {
+		int[] arr = {0};
+		int k = 0;
+		int expected = -1;
+		int actual = _2012_03_16_Prob1_3_FindFirstLarger.findFirstLarger2(arr, k);
 		assertEquals(expected, actual);
 	}
 }
